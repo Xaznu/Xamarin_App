b23558aa228ed8320b22cb76c24b121444f57ee4
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-04 18:19:17 +0200

    MasterDetailPage

---
+++ Aplikacja_mobilna/HelloWorld/HelloWorld/Models/MenuItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HelloWorld.Models
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public Type PageType { get; set; }
+        public MenuItemType Type { get; set; }
+
+        protected MenuItem()
+        {
+        }
+
+        public MenuItem(MenuItemType menuType, string title, Type pageType) : this()
+        {
+            Type = menuType;
+            Title = title;
+            PageType = pageType;
+        }
+    }
+
+    public enum MenuItemType
+    {
+        Sport,
+        SUV,
+        Sedan
+    }
+}
