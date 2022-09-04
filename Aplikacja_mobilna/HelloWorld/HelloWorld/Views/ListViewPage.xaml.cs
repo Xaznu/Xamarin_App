333533a67b010a4aad8e13f6fbafbe554183c85b
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-04 17:02:17 +0200

    ListView

---
+++ Aplikacja_mobilna/HelloWorld/HelloWorld/Views/ListViewPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage : ContentPage
+    {
+        public ListViewPage()
+        {
+            InitializeComponent();
+
+            List<Car> carList = new List<Car>
+            {
+                new Car{ Name = "Audi A4", Year = 2010},
+                new Car{ Name = "Mercedes CLA", Year = 2012},
+                new Car{ Name = "Ford Kuga", Year = 2018},
+                new Car{ Name = "Ferrari California", Year = 2015},
+                new Car{ Name = "Lamborghini Gallardo", Year = 2006},
+                new Car{ Name = "BMW X5", Year = 2020},
+            };
+
+            CarListView.ItemsSource = carList;
+        }
+    }
+}
