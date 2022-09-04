b4986fa6f397f2fe1e282e6e3902508c69b43d35
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-04 18:02:31 +0200

    Ikonka aplikacji + edycja MainActivity.cs

--- Aplikacja_mobilna/HelloWorld/HelloWorld.Android/MainActivity.cs
+++ Aplikacja_mobilna/HelloWorld/HelloWorld.Android/MainActivity.cs
@@ -3,15 +3,25 @@
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace HelloWorld.Droid
 {
-    [Activity(Label = "HelloWorld", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "HelloWorld", Icon = "@drawable/icon_xamarin", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            Forms.SetFlags("SwipeView_Experimental");
+
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
