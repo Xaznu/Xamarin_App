cdf9a1866fd63d1dd538225aeb14bd3d37d40b06
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-05 21:53:12 +0200

    Operacje na bazie danych - kontynuacja

--- Aplikacja_mobilna/HelloWorld/HelloWorld/Models/Car.cs
+++ Aplikacja_mobilna/HelloWorld/HelloWorld/Models/Car.cs
@@ -1,8 +1,23 @@
-﻿namespace HelloWorld.Models
+﻿using SQLite;
+using Xamarin.Forms;
+
+namespace HelloWorld.Models
 {
+    [Table("cars")]
     public class Car
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Year { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+        public CarType Type { get; set; }
+    }
+    public enum CarType
+    {
+        SUV,
+        Sport,
+        Sedan
     }
 }
